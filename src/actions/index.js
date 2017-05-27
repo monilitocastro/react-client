f7471836df4c55283479f93b2e9fb9fa7f677e30
Monilito Castro <mannycastro.epic@gmail.com>
2017-05-27 12:02:30 -0500

    SCAFFOLD signInUser prototype

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+export function signInUser( { email, password } ){
+    return function(dispatch){
+        // submit email/password to server
+
+        // if good then 
+        // authentication is flagged as good,
+        // save JWT token
+        // redirect to secured resource
+
+        // if auth is bad then
+        // present error message
+    }
+        
+}
